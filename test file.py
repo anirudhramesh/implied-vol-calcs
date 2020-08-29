1d70478d283a2aa657873a1d2fadaea84a5f2d85
anirudhramesh <anirudhan91@gmail.com>
2020-08-28 22:56:43 -0400

    writing calculated implied vol for testing binomial later

--- test file.py
+++ test file.py
@@ -5,9 +5,11 @@ import pandas as pd
 
 def main():
     data = pd.read_csv('data.csv', parse_dates=['Date', 'ExpiryDate'])
-
     col_renames = {'CallPut': 'Type', 'Close': 'Spot', 'Strike': 'StrikePrice'}
-    module2.fast_eur_binomial_option_price_wrapper(data)
+    data.rename(columns=col_renames, inplace=True)
+
+    data['impl_vol_guess'] = module1.faster_vol_calc(data).rename('impl_vol_guess')
+    data.to_csv('data.csv', index=False)
 
 
 if __name__ == '__main__':
